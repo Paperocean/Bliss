fa6bb17759743591ba82dc6ee837b56ac09a3322
Michał Wróblewski <157585200+Paperocean@users.noreply.github.com>
2024-10-12 10:35:13 +0000

    Update CSS to fix background behavior and center container on homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import EventPage from './eventpage/Eventpage';
 
 const App = () => {
   return (
+    <div classname="app">
     <Router>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/login" element={<EventPage />} />
       </Routes>
     </Router>
+    </div>
   );
 };
 
