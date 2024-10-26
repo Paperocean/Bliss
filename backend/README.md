dca7a1d884b74fd20a156978044d5386bcf40052
mariasz03 <mariaszmikolaj@gmail.com>
2024-10-26 21:36:51 +0200

    Added tables to init.sql and added Docker to Github Actions

--- backend/README.md
+++ backend/README.md
@@ -1,16 +1,18 @@
-# To build and start Docker containers (defined in docker-compose.yml file):
+# Docker Setup Guide
+
+To build and start Docker containers (defined in docker-compose.yml file):
 ```bash
 docker-compose up --build
 ```
 
-# To find postgres-container IP:
+To find postgres-container IP:
 ```bash
 docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' <name_of_container_running_postgres>
 ```
 
 With our `docker-compose.yml` container's name should be `postgres`.
 
-# To connect to PostgreSQL database running inside Docker container:
+To connect to PostgreSQL database running inside Docker container:
 ```bash
 psql -h <container_ip> -U <db_username> -d <db_name>
 ```
--- backend/init.sql
+++ backend/init.sql
@@ -3,9 +3,47 @@
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(100),
-    email VARCHAR(100) UNIQUE
+    username VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users (username, email) VALUES
-    ('john_doe', 'john@example.com');
+INSERT INTO users (username, email, password_hash) VALUES
+    ('john_doe', 'john@example.com', 'example_password');
+
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    location VARCHAR(255),
+    event_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE ticket_types (
+    id SERIAL PRIMARY KEY,
+    event_id INT REFERENCES events(id) ON DELETE CASCADE,
+    type_name VARCHAR(100) NOT NULL,
+    price DECIMAL (10, 2) NOT NULL,
+    quantity INT NOT NULL, -- Number of tickets available
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(id) ON DELETE CASCADE,
+    ticket_type_id INT REFERENCES ticket_types(id) ON DELETE SET NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE payments (
+    id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(id) ON DELETE CASCADE,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    amount DECIMAL(10, 2),
+    payment_method VARCHAR(50),
+    payment_status VARCHAR(50) DEFAULT 'Pending'
+);
